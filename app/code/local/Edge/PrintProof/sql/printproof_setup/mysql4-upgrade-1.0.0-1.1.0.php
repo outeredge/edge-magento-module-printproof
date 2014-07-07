0bb62a47c28742e318af22e43a6658bd32935ddc
Minlare <mike@outeredgeuk.com>
2014-07-07 15:30:52 +0100

    added order statuses to all states

--- app/code/local/Edge/PrintProof/sql/printproof_setup/mysql4-upgrade-1.0.0-1.1.0.php
+++ app/code/local/Edge/PrintProof/sql/printproof_setup/mysql4-upgrade-1.0.0-1.1.0.php
@@ -5,15 +5,27 @@ $this->startSetup();
 $awaitingApproval = Mage::getModel('sales/order_status');
 $awaitingApproval->setStatus('awaiting_client_proof_approval')
     ->setLabel('Awaiting Client Proof Approval')
+    ->assignState(Mage_Sales_Model_Order::STATE_NEW)
     ->assignState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT)
     ->assignState(Mage_Sales_Model_Order::STATE_PROCESSING)
+    ->assignState(Mage_Sales_Model_Order::STATE_COMPLETE)
+    ->assignState(Mage_Sales_Model_Order::STATE_CLOSED)
+    ->assignState(Mage_Sales_Model_Order::STATE_CANCELED)
+    ->assignState(Mage_Sales_Model_Order::STATE_HOLDED)
+    ->assignState(Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW)
     ->save();
 
 $proofApproved = Mage::getModel('sales/order_status');
 $proofApproved->setStatus('proofs_approved')
     ->setLabel('Proofs Approved')
+    ->assignState(Mage_Sales_Model_Order::STATE_NEW)
     ->assignState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT)
     ->assignState(Mage_Sales_Model_Order::STATE_PROCESSING)
+    ->assignState(Mage_Sales_Model_Order::STATE_COMPLETE)
+    ->assignState(Mage_Sales_Model_Order::STATE_CLOSED)
+    ->assignState(Mage_Sales_Model_Order::STATE_CANCELED)
+    ->assignState(Mage_Sales_Model_Order::STATE_HOLDED)
+    ->assignState(Mage_Sales_Model_Order::STATE_PAYMENT_REVIEW)
     ->save();
 
 $this->endSetup();
