dbfbcf55ba688fd60108a88a5c77fd1449fc2d1d
Bruno Dias <bruno@outeredgeuk.com>
2015-06-03 16:20:42 +0100

    Add followup improvements

--- app/code/local/Edge/PrintProof/controllers/AdminController.php
+++ app/code/local/Edge/PrintProof/controllers/AdminController.php
@@ -11,52 +11,47 @@ class Edge_PrintProof_AdminController extends Mage_Adminhtml_Controller_Action
         $this->getResponse()->setBody($html);
     }
 
-    public function createProofAction()
-    {
-        $params = $this->getRequest()->getParams();
-        $proof = Mage::getModel('printproof/proof');
-        $admin = Mage::getSingleton('admin/session')->getUser();
-
-        if (Mage::getStoreConfig('printproof/general/companyname')) {
-            $name = Mage::getStoreConfig('general/store_information/name');
-        } else {
-            $name = $admin->getFirstname() . ' ' . $admin->getLastname();
-        }
-        
-        $comment = array(
-            'admin' => 1,
-            'name' => $name,
-            'date' => time()
-        );
-        if (isset($params['comment'])){
-            $comment['comment'] = $params['comment'];
-        }
-        if (!empty($_FILES) && isset($_FILES['attachment'])){
-            $attachment = Mage::helper('printproof')->saveAttachment();
-            if ($attachment){
-                $comment['attachment'] = $attachment;
-            }
-        }
-
-        $proof->setOrderId($params['order_id']);
-        $proof->setItemId($params['item_id']);
-        $proof->setComments(serialize(array($comment)));
-        $proof->save();
-
-        Mage::dispatchEvent('printproof_create_adminhtml', array('proof' => $proof));
-        $this->_redirect('adminhtml/sales_order/view', array('order_id'  => $params['order_id']));
-        return;
-    }
-
     public function addToExistingAction()
     {
         $params = $this->getRequest()->getParams();
-        if (Mage::helper('printproof')->addToExisting($params)){
+        if (Mage::helper('printproof')->addToExisting($params)) {
             $this->_redirect('adminhtml/sales_order/view', array('order_id'  => $params['order_id']));
             return;
         }
 
         Mage::getSingleton('adminhtml/session')->addError('An error occured whilst attempting to update the proof.');
+        $this->_redirect('adminhtml/sales_order/view', array('order_id'  => $params['order_id']));
+        return;
+    }
+
+    public function approveProofAction()
+    {
+        $params = $this->getRequest()->getParams();
+        $proof  = Mage::getModel('printproof/proof');
+        $admin  = Mage::getSingleton('admin/session')->getUser();
+
+        $proof->load($params['proof_id']);
+        $proof->setApproved(true);
+        $proof->setApprovedDate(time());
+        $proof->save();
+
+        Mage::dispatchEvent('printproof_approved_customer', array('proof' => $proof));
+        $this->_redirect('adminhtml/sales_order/view', array('order_id'  => $params['order_id']));
+        return;
+    }
+
+    public function rejectProofAction()
+    {
+        $params = $this->getRequest()->getParams();
+        $proof  = Mage::getModel('printproof/proof');
+        $admin  = Mage::getSingleton('admin/session')->getUser();
+
+        $proof->load($params['proof_id']);
+        $proof->setRejected(true);
+        $proof->setRejectedDate(time());
+        $proof->save();
+
+        Mage::dispatchEvent('printproof_rejected_customer', array('proof' => $proof));
         $this->_redirect('adminhtml/sales_order/view', array('order_id'  => $params['order_id']));
         return;
     }
